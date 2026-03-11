d5ab2d2dbaa2d96099a42bed8d21145985bdf525
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 23:52:38 +0530

    integrating deploy script with helperconfig

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,18 +4,26 @@ pragma solidity ^0.8.18;
 import {DecentralisedStableCoin} from "../src/DecentralisedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeploDSC is Script {
 
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (DSCEngine , DecentralisedStableCoin) {
         HelperConfig helperConfig = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
 
-        
         vm.startBroadcast();
         DecentralisedStableCoin dsc = new DecentralisedStableCoin();
-        DSCEngine dscEngine = new DSCEngine();
+        DSCEngine dscEngine = new DSCEngine(tokenAddresses , priceFeedAddresses , address(dsc));
         vm.stopBroadcast();
+        return (dscEngine , dsc);
     }
 }
 
