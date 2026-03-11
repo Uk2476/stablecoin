037518f74d125d3f0e7238241cf2b52c56cab7d6
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 14:29:12 +0530

    Setup function

--- test/DecentralisedStableCoin.t.sol
+++ test/DecentralisedStableCoin.t.sol
@@ -6,6 +6,14 @@ import {Test} from "forge-std/Test.sol";
 
 contract DecentralisedStableCoinTest is Test {
     DecentralisedStableCoin public Dsc ;
+    address User = makeAddr("User");
+
+    function setUp() public {
+        Dsc = new DecentralisedStableCoin();
+        vm.deal(User, 100 ether);
+        vm.deal(msg.sender, 100 ether);
+    }
 
 
+    
 }
