bde54a1ea54151595f6f66d76ac25e4646bc4d25
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 14:26:05 +0530

    test contract for decentralisedStablecoin

---
+++ test/DecentralisedStableCoin.t.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {DecentralisedStableCoin} from "../src/DecentralisedStableCoin.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract DecentralisedStableCoinTest is Test {
+    DecentralisedStableCoin public Dsc ;
+
+
+}
