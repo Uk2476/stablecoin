4e40400d44e77a5a8ffa0e0fb17a2366ac80d5e1
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 19:39:20 +0530

    defined modifier more than zero and validCollateral

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -5,8 +5,23 @@ import {DecentralisedStableCoin} from "./DecentralisedStableCoin.sol";
 
 contract DSCEngine {
     DecentralisedStableCoin public immutable i_dsc;
+    address[] public immutable i_tokenaddresses;
 
     mapping(address token => address pricefeeds) public s_priceFeeds;
+
+    modifier moreThanZero(uint256 amount){
+        if(amount == 0) {
+            revert dsc_amountmustbegreaterthanzero();
+        }
+        _;
+    }
+
+    modifier validCollateral (address token){
+        if(s_priceFeeds[token] == address(0)){
+            revert dsc_notavalidcollateral();
+        }
+        _;
+    }
 
     constructor(address[] memory tokens , address[] memory pricefeeds , address dscAddress) {
         if(tokens.length != pricefeeds.length ){
@@ -14,11 +29,13 @@ contract DSCEngine {
         }
         for(uint256 i = 0 ; i < tokens.length ; i++){
             s_priceFeeds[tokens[i]] = pricefeeds[i];
+            i_tokenaddresses.push(tokens[i]);
         }
         i_dsc = DecentralisedStableCoin(dscAddress);
+        
     }
 
-    function depositCollateral() external {
+    function depositCollateral(address collateralDepositAddress , uint256 amount) external {
         // this function is used to deposit collateral of diiferent token address for a particular address ,
         // for this you have to create two mapping 
         // and connect it with ierc20 , add some modifiers also 
