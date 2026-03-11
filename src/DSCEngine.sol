a3751a55dbd64a26fee17dca8841f0824aa052d5
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 19:25:24 +0530

    constructed constructor

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -6,7 +6,23 @@ import {DecentralisedStableCoin} from "./DecentralisedStableCoin.sol";
 contract DSCEngine {
     DecentralisedStableCoin public immutable i_dsc;
 
-    constructor(address dscAddress) {
+    mapping(address token => address pricefeeds) public s_priceFeeds;
+
+    constructor(address[] memory tokens , address[] memory pricefeeds , address dscAddress) {
+        if(tokens.length != pricefeeds.length ){
+            revert dsc_tokensandpricefeedslengthmismatcch();
+        }
+        for(uint256 i = 0 ; i < tokens.length ; i++){
+            s_priceFeeds[tokens[i]] = pricefeeds[i];
+        }
         i_dsc = DecentralisedStableCoin(dscAddress);
     }
+
+    function depositCollateral() external {
+        // this function is used to deposit collateral of diiferent token address for a particular address ,
+        // for this you have to create two mapping 
+        // and connect it with ierc20 , add some modifiers also 
+    }
+
+
 }
