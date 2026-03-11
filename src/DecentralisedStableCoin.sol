cea9a2d150c193fd4ee8c16a1080f422eea28aaf
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 14:12:12 +0530

    added conditions for mint function

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {ERC20Burnable, ERC20} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract DecentralisedStableCoin is ERC20Burnable, Ownable {
+abstract contract DecentralisedStableCoin is ERC20Burnable, Ownable {
 
     constructor() ERC20("DecentralisedStableCoin" , "DSC"){
     
