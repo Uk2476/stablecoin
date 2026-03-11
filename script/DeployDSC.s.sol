d7743577a3070a233d8638c8fbad5e5f99e25591
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 23:16:53 +0530

    function run

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -7,8 +7,15 @@ import {Script} from "forge-std/Script.sol";
 
 contract DeploDSC is Script {
 
-    function run() external {
+    function run() external returns (DSCEngine , DecentralisedStableCoin) {
+        HelperConfig helperConfig = new HelperConfig();
+
+
         
+        vm.startBroadcast();
+        DecentralisedStableCoin dsc = new DecentralisedStableCoin();
+        DSCEngine dscEngine = new DSCEngine();
+        vm.stopBroadcast();
     }
 }
 
