38252b1dcd8a1bb253c101969008a77c0f147849
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 13:49:06 +0530

    contract for custom stable coin

---
+++ src/DecentralisedStableCoin.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract DecentralisedStableCoin is ERC20Burnable, Ownable {
+
+    
+
+}
