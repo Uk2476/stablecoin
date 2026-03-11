a7d090381a364ea5724ccc88609af14d78628ed9
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 23:32:30 +0530

    defined function to get sepolia eth config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,7 +13,7 @@ contract HelperConfig is Script {
         address wbtcUsdPriceFeed;
         address weth;
         address wbtc;
-        uint256 deployKey;
+        uint256 deployerKey;
     }
 
     constructor (){
@@ -22,5 +22,18 @@ contract HelperConfig is Script {
         } else {
             activeNetworkConfig = getOrCreateAnvilConfig();
         }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaConfig){
+        sepoliaConfig = NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });        
+    }
+
+    function getOrCreateAnvilConfig() public returns (NetworkConfig memory anvilConfig){
     }
 }
