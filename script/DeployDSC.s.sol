3177a2fd65ec43e3027f5c685d84027942542f94
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 23:13:47 +0530

    Deployment contract

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {DecentralisedStableCoin} from "../src/DecentralisedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeploDSC is Script {
+
+    function run() external {
+        
+    }
+}
+
