88765c2c414b677b20645fdb84987286f9d48723
Uk2476 <kuttkarsh028@gmail.com>
2026-03-11 23:49:05 +0530

    fuction to create anvilconfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import { MockV3Aggregator } from "../test/mocks/MockV3Aggregator.sol";
+import { ERC20Mock } from "../test/mocks/ERC20Mock.sol";
 
 contract HelperConfig is Script {
 
     NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;    
 
     struct NetworkConfig{
         address wethUsdPriceFeed;
@@ -26,7 +30,7 @@ contract HelperConfig is Script {
 
     function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaConfig){
         sepoliaConfig = NetworkConfig({
-            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, 
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
             wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
@@ -35,5 +39,25 @@ contract HelperConfig is Script {
     }
 
     function getOrCreateAnvilConfig() public returns (NetworkConfig memory anvilConfig){
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)){
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        //FOR ETHER
+        MockV3Aggregator wethUsdPriceFeed = new MockV3Aggregator(DECIMALS , 1000e8);
+        ERC20Mock weth = new ERC20Mock("WETH" , "WETH" , msg.sender , 1000e18);
+        //FOR BITCOIN
+        MockV3Aggregator wbtcUsdPriceFeed = new MockV3Aggregator(DECIMALS , 10000e8);
+        ERC20Mock wbtc = new ERC20Mock("WBTC" , "WBTC" , msg.sender , 1000e8);
+
+        anvilConfig = NetworkConfig({
+            wethUsdPriceFeed: address(wethUsdPriceFeed),
+            wbtcUsdPriceFeed: address(wbtcUsdPriceFeed),
+            weth: address(weth),
+            wbtc: address(wbtc),
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
+        });
+        vm.stopBroadcast();
     }
 }
