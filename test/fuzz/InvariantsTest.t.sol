247cd55e7f033bdd164c2095540694e16c9000de
Uk2476 <kuttkarsh028@gmail.com>
2026-03-12 20:16:44 +0530

    integrated with handler

--- test/fuzz/InvariantsTest.t.sol
+++ test/fuzz/InvariantsTest.t.sol
@@ -11,6 +11,7 @@ import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DecentralisedStableCoin} from "../../src/DecentralisedStableCoin.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "../fuzz/Handler.t.sol";
 
 contract InvariantsTest is StdInvariant , Test {
 
@@ -24,13 +25,17 @@ contract InvariantsTest is StdInvariant , Test {
     function setUp() external{
         deployer = new DeployDSC();
         (dscEngine , dsc, config) = deployer.run();
-        targetContract(address(dscEngine));
+        Handler handler = new Handler(dscEngine , dsc);
+        targetContract(address(handler));
         (,,weth,wbtc,) = config.activeNetworkConfig();
     }
 
     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view{
         uint256 totalSupply = dsc.totalSupply();
-        uint256 total
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dscEngine));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dscEngine));
+        uint256 totalValueDeposited = (totalWethDeposited * dscEngine.getPriceinUsd(weth) / 1e18) + (totalWbtcDeposited * dscEngine.getPriceinUsd(wbtc) / 1e18);
+        assert(totalValueDeposited >= totalSupply);
 
     }
 }
