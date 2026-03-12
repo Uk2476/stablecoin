1b5a5a38280eba6443be5e23f2fda6ff9a0ee8d2
Uk2476 <kuttkarsh028@gmail.com>
2026-03-12 08:44:55 +0530

    test file for dsc engine

---
+++ test/DSCEngine.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {DecentralisedStableCoin} from "../src/DecentralisedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "../script/DeployDSC.s.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+import { ERC20Mock } from "../test/mocks/ERC20Mock.sol";
+
+
+
+contract DSCEngineTest is Test {
+
+    DSCEngine public dscEngine;
+    DecentralisedStableCoin public dsc;
+    HelperConfig public Config;
+    DeployDSC public deployer;
+
+    address public etherUsdPriceFeed;
+    address public wbtcUsdPriceFeed;
+    address public weth;
+    address public wbtc;    
+
+    address public user = makeAddr("User");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_BALANCE= 10 ether;
+
+    function setUp() public{
+        deployer = new DeployDSC();
+        (dscEngine , dsc , Config) = deployer.run();
+        (etherUsdPriceFeed , wbtcUsdPriceFeed , weth , wbtc , ) = Config.activeNetworkConfig();
+        ERC20Mock(weth).mint(user , STARTING_BALANCE);
+        ERC20Mock(wbtc).mint(user , STARTING_BALANCE);
+
+    }
+
+    function testGetUsdValue() public{
+        
+    }   
+}
